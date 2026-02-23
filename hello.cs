4ae692e01a99bab334f7bebd9f096c9edb01f85d
Leanstix <leanstixx@gmail.com>
2026-02-23 21:12:30 +0100

    first c# commit

---
+++ hello.cs
@@ -0,0 +1,12 @@
+// This is a simple Hello World program in C#
+using System;
+
+class Program
+{
+    // Main method - entry point of the program
+    static void Main()
+    {
+        // Print "Hello World" to the console
+        Console.WriteLine("Hello World");
+    }
+}
